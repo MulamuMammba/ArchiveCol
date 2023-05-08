5fb1eed18b0d7ad9281a2cdd647686770e4e02ba
Mulamu <84395540+DevMulamu@users.noreply.github.com>
2023-05-08 10:24:35 +0200

    added button

--- app/src/main/java/com/example/archivecol/MainActivity.kt
+++ app/src/main/java/com/example/archivecol/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.archivecol
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -14,39 +15,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val addCategory = findViewById<Button>(R.id.addCategoryButton)
 
-        fun createCategory(categoryName: String, goalNumber: Int) {
-            val categoriesRef = FirebaseDatabase.getInstance().getReferenceFromUrl("gs://archivecol.appspot.com")
-            val categoryId = categoriesRef.push().key
-            if (categoryId != null) {
-                val category = hashMapOf("name" to categoryName, "goal" to goalNumber)
-                categoriesRef.child(categoryId).setValue(category)
-            }
-        }
-
-        class CategoryInputDialog(context: Context) {
-
-            private val dialogView = LayoutInflater.from(context).inflate(
-                R.layout.category_input_dialog, null
-            )
-            private val categoryInput = dialogView.findViewById<EditText>(R.id.category_input)
-            private val goalInput = dialogView.findViewById<EditText>(R.id.goal_input)
-
-            private val dialog = AlertDialog.Builder(context)
-                .setTitle("Create category")
-                .setView(dialogView)
-                .setPositiveButton("Create") { _, _ ->
-                    val categoryName = categoryInput.text.toString()
-                    val goalNumber = goalInput.text.toString().toIntOrNull() ?: 0
-                    if (categoryName.isNotEmpty()) {
-                        createCategory(categoryName, goalNumber)
-                    }
-                }
-                .setNegativeButton("Cancel", null)
-                .create()
-
-            private fun createCategory(categoryName: String, goalNumber: Int) {
-                val categoriesRef = FirebaseDatabase.getInstance().getReferenceFromUrl("gs://archivecol.appspot.com")
+        addCategory.setOnClickListener {
+            fun createCategory(categoryName: String, goalNumber: Int) {
+                val categoriesRef = FirebaseDatabase.getInstance()
+                    .getReferenceFromUrl("gs://archivecol.appspot.com")
                 val categoryId = categoriesRef.push().key
                 if (categoryId != null) {
                     val category = hashMapOf("name" to categoryName, "goal" to goalNumber)
@@ -54,10 +28,38 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-            fun show() {
-                dialog.show()
-            }
-        }
+            class CategoryInputDialog(context: Context) {
 
+                private val dialogView = LayoutInflater.from(context).inflate(
+                    R.layout.category_input_dialog, null
+                )
+                private val categoryInput = dialogView.findViewById<EditText>(R.id.category_input)
+                private val goalInput = dialogView.findViewById<EditText>(R.id.goal_input)
+
+                private val dialog = AlertDialog.Builder(context)
+                    .setTitle("Create category")
+                    .setView(dialogView)
+                    .setPositiveButton("Create") { _, _ ->
+                        val categoryName = categoryInput.text.toString()
+                        val goalNumber = goalInput.text.toString().toIntOrNull() ?: 0
+                        if (categoryName.isNotEmpty()) {
+                            createCategory(categoryName, goalNumber)
+                        }
+                    }
+                    .setNegativeButton("Cancel", null)
+                    .create()
+
+                private fun createCategory(categoryName: String, goalNumber: Int) {
+                    val categoriesRef = FirebaseDatabase.getInstance()
+                        .getReferenceFromUrl("gs://archivecol.appspot.com")
+                    val categoryId = categoriesRef.push().key
+                    if (categoryId != null) {
+                        val category = hashMapOf("name" to categoryName, "goal" to goalNumber)
+                        categoriesRef.child(categoryId).setValue(category)
+                    }
+                }
+            }
+
+        }
     }
 }
